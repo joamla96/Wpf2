c62e6158bfbba0d89427ff361d6eeddebe9ef68e
jona8690 <jona8690@edu.eal.dk>
2017-01-24 09:03:04 +0100

    Renamed more controllers

--- Wpf2/MainWindow.xaml.cs
+++ Wpf2/MainWindow.xaml.cs
@@ -20,50 +20,50 @@ namespace Wpf2
     /// </summary>
     public partial class MainWindow : Window
     {
-        Controller controler;
+        Controller controller;
         public MainWindow()
         {
-            controler = Controller.GetInstance();
+            controller = Controller.GetInstance();
             InitializeComponent();
 
 			UpdateInfo();
 		}
 
 		private void UpdateInfo() {
-			count.Content = controler.PersonCount;
-			index.Content = controler.PersonIndex;
+			count.Content = controller.PersonCount;
+			index.Content = controller.PersonIndex;
 
-			textBox_firstname.Text = controler.CurentPerson.FirstName;
-			textBox_lastname.Text = controler.CurentPerson.LastName;
-			textBox_age.Text = controler.CurentPerson.Age + "";
-			textBox_telnr.Text = controler.CurentPerson.TelephoneNr;
+			textBox_firstname.Text = controller.CurentPerson.FirstName;
+			textBox_lastname.Text = controller.CurentPerson.LastName;
+			textBox_age.Text = controller.CurentPerson.Age + "";
+			textBox_telnr.Text = controller.CurentPerson.TelephoneNr;
 		}
 
 		private void button_new_Click(object sender, RoutedEventArgs e) {
 			int age;
 			int.TryParse(textBox_age.Text, out age);
 
-			controler.CurentPerson.FirstName = textBox_firstname.Text;
-			controler.CurentPerson.LastName = textBox_lastname.Text;
-			controler.CurentPerson.Age = age;
-			controler.CurentPerson.TelephoneNr = textBox_telnr.Text;
+			controller.CurentPerson.FirstName = textBox_firstname.Text;
+			controller.CurentPerson.LastName = textBox_lastname.Text;
+			controller.CurentPerson.Age = age;
+			controller.CurentPerson.TelephoneNr = textBox_telnr.Text;
 
-			controler.AddPerson();
+			controller.AddPerson();
 			UpdateInfo();
 		}
 
 		private void button_delete_Click(object sender, RoutedEventArgs e) {
-			controler.DeletePerson();
+			controller.DeletePerson();
 			UpdateInfo();
 		}
 
 		private void button_up_Click(object sender, RoutedEventArgs e) {
-			controler.NextPerson();
+			controller.NextPerson();
 			UpdateInfo();
 		}
 
 		private void button_down_Click(object sender, RoutedEventArgs e) {
-			controler.PrevPerson();
+			controller.PrevPerson();
 			UpdateInfo();
 		}
 	}
